27a163884e8c6281d1a16ce9a1f201d8e6e19c95
jizhuoran <jizr@connect.hku.hk>
2018-11-26 01:18:13 +0800

    clean code

---
+++ third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/arena.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/arena.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/arenastring.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/arenastring.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/extension_set.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/extension_set.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/generated_message_util.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/generated_message_util.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/io/coded_stream.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/io/coded_stream.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/io/zero_copy_stream.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/io/zero_copy_stream.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/io/zero_copy_stream_impl_lite.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/io/zero_copy_stream_impl_lite.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/message_lite.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/message_lite.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/repeated_field.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/repeated_field.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/atomicops_internals_x86_gcc.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/atomicops_internals_x86_gcc.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/atomicops_internals_x86_msvc.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/atomicops_internals_x86_msvc.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/bytestream.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/bytestream.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/common.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/common.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/int128.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/int128.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/once.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/once.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/status.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/status.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/statusor.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/statusor.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/stringpiece.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/stringpiece.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/stringprintf.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/stringprintf.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/structurally_valid.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/structurally_valid.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/strutil.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/strutil.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/time.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/stubs/time.cc.o"
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/wire_format_lite.cc" "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/.cbuild/CMakeFiles/libprotobuf-lite.dir/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src/google/protobuf/wire_format_lite.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GOOGLE_PROTOBUF_CMAKE_BUILD"
+  "HAVE_PTHREAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/zrji/android_caffe/caffe-android-opencl/third_party/protobuf-3.1.0/src"
+  "/home/zrji/android_caffe/tmp_ndk/android-ndk-r18b/sources/cxx-stl/llvm-libc++/include"
+  "/home/zrji/android_caffe/tmp_ndk/android-ndk-r18b/sources/cxx-stl/llvm-libc++abi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
