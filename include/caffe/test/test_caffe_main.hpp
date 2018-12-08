2fe5c490b93499e6a51520981dfad9db3573946e
jizhuoran <jizr@connect.hku.hk>
2018-12-08 23:45:01 +0800

    add test

---
+++ include/caffe/test/test_caffe_main.hpp
@@ -0,0 +1,81 @@
+// The main caffe test code. Your test cpp code should include this hpp
+// to allow a main function to be compiled into the binary.
+#ifndef CAFFE_TEST_TEST_CAFFE_MAIN_HPP_
+#define CAFFE_TEST_TEST_CAFFE_MAIN_HPP_
+
+#ifdef USE_GLOG
+#include <glog/logging.h>
+#else
+#include "caffe/glog_wrapper.hpp"
+#endif
+
+#include <gtest/gtest.h>
+
+#include <cstdio> 
+#include <cstdlib>
+
+#include "caffe/common.hpp"
+
+using std::cout;
+using std::endl;
+
+#ifdef CMAKE_BUILD
+  #include "caffe_config.h"
+#else
+  #define CUDA_TEST_DEVICE -1
+  #define EXAMPLES_SOURCE_DIR "examples/"
+  #define ABS_TEST_DATA_DIR "src/caffe/test/test_data"
+#endif
+
+int main(int argc, char** argv);
+
+namespace caffe {
+
+template <typename TypeParam>
+class MultiDeviceTest : public ::testing::Test {
+ public:
+  typedef typename TypeParam::Dtype Dtype;
+ protected:
+  MultiDeviceTest() {
+    Caffe::set_mode(TypeParam::device);
+  }
+  virtual ~MultiDeviceTest() {}
+};
+
+typedef ::testing::Types<float> TestDtypes;
+
+template <typename TypeParam>
+struct CPUDevice {
+  typedef TypeParam Dtype;
+  static const Caffe::Brew device = Caffe::CPU;
+};
+
+template <typename Dtype>
+class CPUDeviceTest : public MultiDeviceTest<CPUDevice<Dtype> > {
+};
+
+#ifdef CPU_ONLY
+
+typedef ::testing::Types<CPUDevice<float> > TestDtypesAndDevices;
+
+#else
+
+template <typename TypeParam>
+struct GPUDevice {
+  typedef TypeParam Dtype;
+  static const Caffe::Brew device = Caffe::GPU;
+};
+
+template <typename Dtype>
+class GPUDeviceTest : public MultiDeviceTest<GPUDevice<Dtype> > {
+};
+
+typedef ::testing::Types<CPUDevice<float>,
+                         GPUDevice<float>, GPUDevice<half> >
+                         TestDtypesAndDevices;
+
+#endif
+
+}  // namespace caffe
+
+#endif  // CAFFE_TEST_TEST_CAFFE_MAIN_HPP_
